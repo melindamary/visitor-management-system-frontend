a55ef82c2a60d57c6887e550ece47d1759c4eae8
Adithya S Susa <adithya.susa@experionglobal.local>
2024-07-22 23:24:37 +0530

    add: modified visitor log service

--- src/app/core/services/visitorLogServices/visitor-log.service.ts
+++ src/app/core/services/visitorLogServices/visitor-log.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { APIResponse } from '../../models/api-response';
 import { VisitorPassCodeDTO } from '../../models/visitor-pass-code-dto';
 
@@ -13,15 +13,44 @@ export class VisitorLogService {
   constructor(private http: HttpClient) {}
 
   getVisitorLogToday(): Observable<APIResponse> {
-    return this.http.get<APIResponse>(`${this.apiUrl}/GetVisitorLogToday`);
+    return this.http.get<any>(`${this.apiUrl}/GetVisitorLogToday`)
+    .pipe(
+      map(response => {
+        // Transform response to match VisitorLogResult structure
+        if (response.isSuccess) {
+          return {
+            ...response,
+            result: {
+              ...response.result,
+              upcomingVisitors: response.result.upcomingVisitors.$values,
+              activeVisitors: response.result.activeVisitors.$values,
+              visitorsToday: response.result.visitorsToday.$values,
+            }
+          };
+        }
+        return response;
+      }),
+      catchError(this.handleError)
+    );
   }
 
   updateCheckInTimeAndCardNumber(id: number, updateVisitorPassCode: VisitorPassCodeDTO): Observable<APIResponse> {
-    return this.http.put<APIResponse>(`${this.apiUrl}/UpdateCheckInTimeAndCardNumber/${id}`, updateVisitorPassCode);
+    return this.http.put<APIResponse>(`${this.apiUrl}/UpdateCheckInTimeAndCardNumber/${id}`, updateVisitorPassCode)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateCheckOutTime(id: number): Observable<APIResponse> {
-    return this.http.put<APIResponse>(`${this.apiUrl}/UpdateCheckOutTime/${id}`, {});
+    return this.http.put<APIResponse>(`${this.apiUrl}/UpdateCheckOutTime/${id}`, {})
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(() => new Error(error.message || 'Server error'));
   }
 }
 
