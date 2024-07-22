f3de5a01ff63190c883517df5ce8208a76063ac5
melindamary <meldweb213@gmail.com>
2024-07-22 21:56:27 +0530

    fix: changed ace-auth guard from function to class

--- src/app/core/guard/auth-ace.guard.ts
+++ src/app/core/guard/auth-ace.guard.ts
@@ -1,16 +1,53 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+// import { inject } from '@angular/core';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { AuthService } from '../services/authServices/auth.service';
+// import { isPlatformBrowser,} from '@angular/common';
+// import { Component, PLATFORM_ID } from '@angular/core';
+
+// export const authAceGuard: CanActivateFn = (route, state) => {
+//   const authService = inject(AuthService);
+//   const router = inject(Router);
+//   const platformId = inject(PLATFORM_ID);
+  
+//   const username = localStorage.getItem('userRole');
+//   if (authService.isLoggedIn() &&  ((username == 'ACE') || (username == 'Admin'))) {
+//     return true;
+//   } else {
+//     router.navigate(['/login']);
+//     return false;
+//   }
+// };
+
+import { Injectable, Inject } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/authServices/auth.service';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
-export const authAceGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthAceGuard implements CanActivate {
 
-  const username = localStorage.getItem('userRole');
-  if (authService.isLoggedIn() &&  ((username == 'ACE') || (username == 'Admin'))) {
-    return true;
-  } else {
-    router.navigate(['/login']);
-    return false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  canActivate(): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      const username = localStorage.getItem('userRole');
+      if (this.authService.isLoggedIn() && (username === 'ACE' || username === 'Admin')) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+    } else {
+      // Handle non-browser environments if necessary
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
-};
+}
