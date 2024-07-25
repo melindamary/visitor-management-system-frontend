bca4c55bdc30e0d3f4ccc834e84692b174898ba3
aiswaryajayadev20 <aiswaryajayadev2000@gmail.com>
2024-07-25 19:32:53 +0530

    Integrated add user with backend

--- src/app/core/services/UserManagementServices/user-management-service.service.ts
+++ src/app/core/services/UserManagementServices/user-management-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
 import { GetIdAndName } from '../../models/getIdAndName.interface';
+import { AddNewUser } from '../../models/addNewUser.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,12 @@ export class UserManagementServiceService {
     return this.http.get<{ $id: string; $values: GetIdAndName[] }>(apiUrl).pipe(
       map(response => response.$values)
     );
+  }
+
+  postNewUser(newUser:AddNewUser):Observable<AddNewUser>{
+    console.log("new user details",newUser);    
+    const apiUrl="https://localhost:7121/User/CreateNewUser";
+     return this.http.post<AddNewUser>(apiUrl,newUser);
+
   }
 }
