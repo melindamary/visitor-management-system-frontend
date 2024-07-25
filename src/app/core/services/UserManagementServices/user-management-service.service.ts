a9cac6c58bf9f960d70586cff847b669c1b18b60
aiswaryajayadev20 <aiswaryajayadev2000@gmail.com>
2024-07-26 00:00:31 +0530

    add user api update

--- src/app/core/services/UserManagementServices/user-management-service.service.ts
+++ src/app/core/services/UserManagementServices/user-management-service.service.ts
@@ -14,14 +14,14 @@ export class UserManagementServiceService {
   constructor(private http:HttpClient) { }
 
   getRoleIdAndName(): Observable<GetIdAndName[]> {
-    const apiUrl = "https://localhost:7121/Role/GetRoleIdAndName/get-role-id-name";
+    const apiUrl = "http://localhost:5292/Role/GetRoleIdAndName/get-role-id-name";
     return this.http.get<{ $id: string; $values: GetIdAndName[] }>(apiUrl).pipe(
       map(response => response.$values)
     );
   }
 
   getLocationIdAndName(): Observable<GetIdAndName[]> {
-    const apiUrl = "https://localhost:7121/Location/GetLocationIdAndName";
+    const apiUrl = "http://localhost:5292/Location/GetLocationIdAndName";
     return this.http.get<{ $id: string; $values: GetIdAndName[] }>(apiUrl).pipe(
       map(response => response.$values)
     );
@@ -29,7 +29,7 @@ export class UserManagementServiceService {
 
   postNewUser(newUser:AddNewUser):Observable<AddNewUser>{
     console.log("new user details",newUser);    
-    const apiUrl="https://localhost:7121/User/CreateNewUser";
+    const apiUrl="http://localhost:5292/User/CreateNewUser";
      return this.http.post<AddNewUser>(apiUrl,newUser);
 
   }
