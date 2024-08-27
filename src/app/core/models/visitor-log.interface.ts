5a9d2178d3ab42ae03cade9d4b27364899c4959b
Adithya S Susa <adithya.susa@experionglobal.local>
2024-08-27 17:38:13 +0530

    modify: visitor details modal design in visitor log

--- src/app/core/models/visitor-log.interface.ts
+++ src/app/core/models/visitor-log.interface.ts
@@ -1,37 +1,47 @@
+export interface Device {
+  id: number;
+  name: string;
+  serialNumber: string;
+}
+export interface DeviceList {
+  $id: string;
+  $values: Device[];
+}
+
 export interface VisitorLog {
-    id: number;
-    name: string;
-    phone: string;
-    purposeName: string;
-    checkInTime: string | null;
-    checkOutTime: string | null;
-    visitorPassCode: string;
-    visitDate: Date | null;
-    deviceName: string | null;
-    hostName: string | null;
-    photoBase64: string | null;
-  }
-  
-  export interface VisitorList {
-    $id: string;
-    $values: VisitorLog[];
-  }
-  
-  export interface VisitorLogResult {
-    activeVisitorsCount: number;
-    totalVisitorsCount: number;
-    checkedOutVisitorsCount: number;
-    upcomingVisitors: VisitorList;
-    activeVisitors: VisitorList;
-    visitorsToday: VisitorList;
-    checkedOutVisitors: VisitorList;
-  }
-  
-  export interface VisitorLogResponse {
-    isSuccess: boolean;
-    result: VisitorLogResult;
-    statusCode: number;
-    errorMessages: string[];
-  }
-  
-  
+  id: number;
+  name: string;
+  phone: string;
+  purposeName: string;
+  checkInTime: string | null;
+  checkOutTime: string | null;
+  visitorPassCode: string;
+  visitDate: Date | null;
+  deviceName: string | null; // Consider removing if not used
+  serialNumber: string | null; // Consider removing if not used
+  hostName: string | null;
+  photoBase64: string | null;
+  devices: DeviceList; // Updated to reflect the nested structure
+}
+
+export interface VisitorList {
+  $id: string;
+  $values: VisitorLog[];
+}
+
+export interface VisitorLogResult {
+  activeVisitorsCount: number;
+  totalVisitorsCount: number;
+  checkedOutVisitorsCount: number;
+  upcomingVisitors: VisitorList;
+  activeVisitors: VisitorList;
+  visitorsToday: VisitorList;
+  checkedOutVisitors: VisitorList;
+}
+
+export interface VisitorLogResponse {
+  isSuccess: boolean;
+  result: VisitorLogResult;
+  statusCode: number;
+  errorMessages: string[];
+}
