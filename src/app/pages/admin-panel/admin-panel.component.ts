4d53dc3feaa30c393616e147f505ced9be61863c
melindamary <meldweb213@gmail.com>
2024-11-24 18:40:42 +0530

    update: modify admin-panel path names

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -3,11 +3,11 @@ import { TabViewModule } from 'primeng/tabview';
 // import { AdminViewUserComponent } from './user-management/components/admin-view-user/admin-view-user.component';
 // import { AdminVisitPurposeTableComponent } from './visit-purpose-management/admin-visit-purpose-table/admin-visit-purpose-table.component';
 // import { AdminViewRoleComponent } from './role-management/admin-view-role/admin-view-role.component';
-import { AdminViewUserComponent } from '../Admin-Panel/user-management/components/admin-view-user/admin-view-user.component';
-import { AdminVisitPurposeTableComponent } from '../Admin-Panel/visit-purpose-management/admin-visit-purpose-table/admin-visit-purpose-table.component';
-import { AdminViewRoleComponent } from '../Admin-Panel/role-management/admin-view-role/admin-view-role.component';
-import { DeviceManagementComponent } from '../Admin-Panel/device-management/device-management.component';
-import { LocationManagementComponent } from '../Admin-Panel/location-management/location-management.component';
+import { AdminViewUserComponent } from '../admin-panel/user-management/components/admin-view-user/admin-view-user.component';
+import { AdminVisitPurposeTableComponent } from '../admin-panel/visit-purpose-management/admin-visit-purpose-table/admin-visit-purpose-table.component';
+import { AdminViewRoleComponent } from '../admin-panel/role-management/admin-view-role/admin-view-role.component';
+import { DeviceManagementComponent } from '../admin-panel/device-management/device-management.component';
+import { LocationManagementComponent } from '../admin-panel/location-management/location-management.component';
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
